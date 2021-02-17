2776cbfb869aa86594d31b7741c6ed61f8923678
NonsoAmadi10 <nonsoamadi@aol.com>
2021-02-17 20:45:54 +0100

    add comment field

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,17 @@ class CommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+
+        redirect_to post_path(@post)
     end
 
     def destroy
+    end
+
+
+    private 
+    def comment_params
+        params.require(:comment).permit(:name, :comment)
     end
 end
